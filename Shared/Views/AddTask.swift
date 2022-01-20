d2f0e9544d98eabdb8134ef3568dbbc0211c8159
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-01-20 12:26:36 -0500

    Created and laid out the view that allows us to create a new reminder

---
+++ Shared/Views/AddTask.swift
@@ -0,0 +1,52 @@
+//
+//  AddTask.swift
+//  RemindersApp (iOS)
+//
+//  Created by Harris-Stoertz, Rowan on 2022-01-20.
+//
+
+import SwiftUI
+
+struct AddTask: View {
+    
+    //details of the new task
+    @State private var description = ""
+    @State private var priority = TaskPriority.low
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    TextField("Description", text: $description)
+                    
+                    Picker("Priority", selection: $priority) {
+                        Text(TaskPriority.low.rawValue).tag(TaskPriority.low)
+                        Text(TaskPriority.medium.rawValue).tag(TaskPriority.medium)
+                        Text(TaskPriority.high.rawValue).tag(TaskPriority.high)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationTitle("New Reminder")
+            .toolbar {
+                ToolbarItem (placement: .primaryAction) {
+                    Button ("Save") {
+                        saveTask()
+                    }
+                }
+            }
+        }
+    }
+    
+    func saveTask() {
+        
+        
+    }
+
+}
+
+struct AddTask_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTask()
+    }
+}
