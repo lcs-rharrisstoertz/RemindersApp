19b6c374ff3160a9b158229b61be11f8a8228ade
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-01-20 11:22:21 -0500

    Initial Commit

---
+++ Shared/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  Shared
+//
+//  Created by Harris-Stoertz, Rowan on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
